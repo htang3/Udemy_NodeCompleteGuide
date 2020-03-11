a1fe8eff481827a3a83bf5bb8ae1247420ef1b36
Hung Tang <hung.tang@sjsu.edu>
2020-03-10 18:38:32 -0700

    update on 3_10_2020

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express(); //app is express application
+
 const errorController = require('./controllers/404')
 //global value, setting the pug
 app.set('view engine', 'ejs');
